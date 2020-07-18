631aee5ceb9d2a11afebd070401e5d148204b64c
Nico <neekutzil@gmail.com>
2020-07-17 19:01:35 -0700

    Added portfolio images, docs, etc

---
+++ assignments/week 5/javascript/scripts/main.js
@@ -0,0 +1,24 @@
+// Math Operators in JavaScript
+// + - / * %
+
+var number1 = 10;
+var number2 = 3;
+
+// Addition
+var sum = number1 + number2;
+
+// Subtraction
+var difference = number1 - number2;
+
+// Multiplication
+var product = number1 + number2;
+
+// Division
+var quotient = number1 / number2;
+
+// Modulo
+var remainder = number1 % number2;
+
+console.log("The numbers used are" + number1 + "and" + number2 );
+// The numbers used are 13
+// The numbers used are 103
